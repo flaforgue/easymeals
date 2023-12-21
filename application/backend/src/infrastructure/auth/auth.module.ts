28064f4b33b19dfd5b5adce7ce6983f984e6ffd5
Florian Laforgue <florian@playplay.com>
2023-12-21 13:54:41 +0100

    Find or create user on login

--- application/backend/src/infrastructure/auth/auth.module.ts
+++ application/backend/src/infrastructure/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '../database/database.module';
 import { JwtStrategy } from './jwt.strategy';
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
@@ -6,6 +7,7 @@ import { PassportModule } from '@nestjs/passport';
 @Module({
   imports: [
     ConfigModule,
+    DatabaseModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
   providers: [JwtStrategy],
--- application/backend/src/infrastructure/auth/jwt.strategy.ts
+++ application/backend/src/infrastructure/auth/jwt.strategy.ts
@@ -2,11 +2,25 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { PrismaService } from '../database/prisma.service';
 import { passportJwtSecret } from 'jwks-rsa';
+
+interface Auth0AccessTokenPayload {
+  iss: string;
+  sub: string;
+  aud: string[];
+  iat: string;
+  exp: string;
+  azp: string;
+  scope: string;
+}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  public constructor(configService: ConfigService) {
+  public constructor(
+    private readonly prismaService: PrismaService,
+    configService: ConfigService,
+  ) {
     super({
       secretOrKeyProvider: passportJwtSecret({
         cache: true,
@@ -21,7 +35,22 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  public validate(payload: unknown): unknown {
-    return payload;
+  public async validate(payload: Auth0AccessTokenPayload) {
+    const idpId = payload.sub;
+    const existingUser = await this.prismaService.user.findFirst({
+      where: {
+        idpId: idpId,
+      },
+    });
+
+    if (existingUser !== null) {
+      return existingUser;
+    }
+
+    return this.prismaService.user.create({
+      data: {
+        idpId: idpId,
+      },
+    });
   }
 }

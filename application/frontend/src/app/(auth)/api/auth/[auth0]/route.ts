c144fbeaf7b9509b679e4e08264e5924766032eb
Florian Laforgue <florian@playplay.com>
2023-12-21 13:54:12 +0100

    restrict access token scope

--- application/frontend/src/app/(auth)/api/auth/[auth0]/route.ts
+++ application/frontend/src/app/(auth)/api/auth/[auth0]/route.ts
@@ -5,7 +5,7 @@ export const GET = handleAuth({
   login: handleLogin({
     authorizationParams: {
       audience: process.env.AUTH0_AUDIENCE,
-      scope: 'openid profile email offline_access',
+      scope: 'openid offline_access',
     },
   }),
 }) as NextApiHandler;

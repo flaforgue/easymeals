cec89d3d9fcf841bccb70d05401df60520de2bde
Florian Laforgue <florian@playplay.com>
2023-12-22 20:53:20 +0100

    Close DeleteModal on outside click

--- application/frontend/src/components/DeleteModal.tsx
+++ application/frontend/src/components/DeleteModal.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import { MouseEvent, useState } from 'react';
 import DisabledOverlay from './DisabledOverlay';
 import SpinningLoader from './SpinningLoader';
-import { useState } from 'react';
 
 interface DeleteModalProps {
   title: string;
@@ -22,13 +22,14 @@ export default function DeleteModal({
     setIsSubmitting(true);
     await onConfirm();
   };
+  const stopPropagation = (e: MouseEvent<HTMLElement>) => e.stopPropagation();
 
   return (
     <div className="relative z-10">
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+      <div onClick={onCancel} className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div onClick={stopPropagation} className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
                 <div
